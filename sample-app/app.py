ff3ff7c8004bce9ca065b1de60dd4e2dcf28edd5
kaustavhalder <khalder.08@gmail.com>
2025-02-02 08:21:00 +0530

    added manifests

---
+++ sample-app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return "Hello World"
+
+
+@app.route('/health')
+def hello_world():
+    return "App is Healthy"
+
+
+if __name__ == "__name__":
+    app.run()
